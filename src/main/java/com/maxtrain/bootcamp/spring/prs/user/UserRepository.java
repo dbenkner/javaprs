797129a3adc0abdbdbc991210a9b84b3ae25ee3a
David Benkner <d.benkner@gmail.com>
2023-11-06 21:32:44 -0500

    backup

--- src/main/java/com/maxtrain/bootcamp/spring/prs/user/UserRepository.java
+++ src/main/java/com/maxtrain/bootcamp/spring/prs/user/UserRepository.java
@@ -2,8 +2,11 @@ package com.maxtrain.bootcamp.spring.prs.user;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
 	Optional<User> findUserByUsernameAndPassword(String username, String password);
+	@Query(value = "select top 1 score from Users Order By score desc", nativeQuery = true)
+	User findTopScore();
 }
